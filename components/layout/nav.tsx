77908a16c6660aa42f54c135fc5b93a3bdb0fa5e
Yudi Xu <xyd945@Yudis-MacBook-Pro.local>
2023-12-27 22:30:38 +0100

    modify the nav.tsx file

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,8 +1,8 @@
 import Navbar from "./navbar";
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { useSession } from "next-auth/react";
 
-export default async function Nav() {
-  const session = await getServerSession(authOptions);
+export default function Nav() {
+  const { data: session } = useSession();
+
   return <Navbar session={session} />;
 }
