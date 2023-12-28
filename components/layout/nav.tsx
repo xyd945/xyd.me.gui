569fbfa444f476fbfa69698649651cf4ab27c1cb
Yudi Xu <xyd945@Yudis-MacBook-Pro.local>
2023-12-28 12:13:51 +0100

    update some files to fix the React Context in Server Components

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -1,8 +1,5 @@
 import Navbar from "./navbar";
-import { useSession } from "next-auth/react";
 
 export default function Nav() {
-  const { data: session } = useSession();
-
-  return <Navbar session={session} />;
+  return <Navbar />;
 }
